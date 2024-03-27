da6527cadb43734aab77bff2159830fc27362ed7
Josh200315 <50426132+Josh200315@users.noreply.github.com>
2024-03-27 17:50:40 -0500

    Revert "Revert "USB mass storage functionality""
    This reverts commit 0f3f3f6f924f70e404841366f9f1528a27ff14cf.

---
+++ src/usb_classes.cpp
@@ -0,0 +1,70 @@
+#include "usb_classes.h"
+
+#include "flashdisk.h"
+#include "cdcusb.h"
+
+#if CFG_TUD_MSC
+
+FlashUSB USBStorage;
+
+template <size_t N>
+constexpr size_t len(const char (&str)[N]) {
+    return N;
+}
+
+const char l1[] = "ffat";
+char lblBuf[len(l1)];
+
+bool initMSC()
+{
+    memcpy(lblBuf, l1, len(l1));
+    if (USBStorage.init("/fat1", lblBuf))
+    {
+        return USBStorage.begin();
+    }
+    return false;
+}
+
+#endif
+
+#if CFG_TUD_CDC
+
+CDCusb USBSerial;
+
+class MyCDCCallbacks : public CDCCallbacks {
+    void onCodingChange(cdc_line_coding_t const* p_line_coding)
+    {
+        int bitrate = USBSerial.getBitrate();
+        Serial.printf("new bitrate: %d\n", bitrate);
+    }
+
+    bool onConnect(bool dtr, bool rts)
+    {
+        Serial.printf("connection state changed, dtr: %d, rts: %d\n", dtr, rts);
+        return true;  // allow to persist reset, when Arduino IDE is trying to enter bootloader mode
+    }
+
+    void onData()
+    {
+        int len = USBSerial.available();
+        Serial.printf("\nnew data, len %d\n", len);
+        uint8_t buf[len] = {};
+        USBSerial.read(buf, len);
+        Serial.write(buf, len);
+    }
+
+    void onWantedChar(char c)
+    {
+        Serial.printf("wanted char: %c\n", c);
+    }
+};
+
+bool initSerial()
+{
+    USBSerial.setCallbacks(new MyCDCCallbacks());
+    USBSerial.setWantedChar('x');
+
+    return USBSerial.begin();
+}
+
+#endif
