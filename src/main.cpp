0f3f3f6f924f70e404841366f9f1528a27ff14cf
Josh200315 <50426132+Josh200315@users.noreply.github.com>
2024-03-27 17:49:40 -0500

    Revert "USB mass storage functionality"
    This reverts commit f569495d9bc63e2b168810e398b06d36803477aa.

--- src/main.cpp
+++ src/main.cpp
@@ -5,82 +5,64 @@
 // #define PRO_FEATURES Only define this for MMPro (handled automatically by platformio config when building project)
 // wrap statements in #ifdef DEBUG #endif to enable them only in debug builds
 #include "sensor.h"
-#include "taskwrapper.h"
+#include "task.h"
 #include "debug.h"
-#include "usb_classes.h"
 
 #ifdef PRO_FEATURES
 #include "display.h"
 
 TFT_Parallel display(320, 170);
-// volatile bool usbConnState = false;
 
 auto displayTask = Task("Display", 5000, 2, +[](){
-    static uint32_t t = 0;
-    while (1) {
-        display.clear();
+  static uint32_t t = 0;
+  while (1) {
+    display.clear();
 
-        // Draw code goes between `display.clear()` and `display.refresh()`
-        display.setCursor(10, 10);
-        display.print("Hello, Mouseless World!");
-        display.setCursor(10, 40);
-        display.printf("Frame %i", ++t);
-        // display.setCursor(10, 70);
-        // display.printf("USB %s", usbConnState ? "Connected" : "Disconnected");
+    // Draw code goes between `display.clear()` and `display.refresh()`
+    display.setCursor(10, 80);
+    display.print("Hello, Mouseless World!");
+    display.setCursor(10, 110);
+    display.printf("Frame %i", ++t);
 
-        display.refresh();
-        while (!display.done_refreshing());
-    }
+    display.refresh();
+  }
 });
 
 #endif
 
 auto imuTask = Task("IMU Polling", 5000, 1, +[](const uint16_t freq){
-    const TickType_t delayTime = pdMS_TO_TICKS(1000 / freq);
-    Orientation cur;
-    while (true) {
-        cur = BNO086::poll();
-        USBSerial.printf("Roll: % 7.2f, Pitch: % 7.2f, Yaw: % 7.2f\n", cur.roll, cur.pitch, cur.yaw);
-        vTaskDelay(delayTime);
-    }
+  const TickType_t delayTime = pdMS_TO_TICKS(1000 / freq);
+  Orientation cur;
+  while (true) {
+    cur = BNO086::poll();
+    Serial.printf("Roll: % 5.2f,\tPitch: % 5.2f,\tYaw: % 5.2f\n", cur.roll, cur.pitch, cur.yaw);
+    vTaskDelay(delayTime);
+  }
 });
 
-// auto usbConnStateTask = Task("USB Connection State Monitoring", 4000, 1, +[](){
-//     while (1) {
-//         usbConnState = USBStorage.isReady();
-//         vTaskDelay(pdMS_TO_TICKS(100));
-//     }
-// });
-
 void setup() {
-    Serial.begin(115200);
-
+  Serial.begin(115200);
+  
 #ifdef DEBUG
-    // while(!Serial) delay(10); // Wait for Serial to become available.
-    // Necessary for boards with native USB (like the SAMD51 Thing+).
-    // For a final version of a project that does not need serial debug (or a USB cable plugged in),
-    // Comment out this while loop, or it will prevent the remaining code from running.
+  while(!Serial) delay(10); // Wait for Serial to become available.
+  // Necessary for boards with native USB (like the SAMD51 Thing+).
+  // For a final version of a project that does not need serial debug (or a USB cable plugged in),
+  // Comment out this while loop, or it will prevent the remaining code from running.
 #endif
 
-    delay(3000);
-
-    initSerial();
-    initMSC();
-    // usbConnStateTask();
+  delay(3000);
 
 #ifdef PRO_FEATURES
-    display.init();
+  display.init();
 
-    display.setTextColor(color_rgb(255, 255, 255));
-    display.setTextSize(2);
+  display.setTextColor(color_rgb(255, 255, 255));
+  display.setTextSize(2);
 
-    displayTask();
+  displayTask();
 #endif
 
-    if (BNO086::init())
-        imuTask(100);
-    else
-        Serial.println("Could not initialize BNO086");
+  msg_assert(BNO086::init(), "Could not initialize BNO086");
+  imuTask(100);
 }
 
 void loop() {
