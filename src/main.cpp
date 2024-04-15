bcd46b7b81f2c03b0a51b9ec7559b2575f6af3c0
Josh200315 <50426132+Josh200315@users.noreply.github.com>
2024-04-15 01:33:13 -0500

    y commits goofy

--- src/main.cpp
+++ src/main.cpp
@@ -40,12 +40,11 @@ TFT_Parallel display(320, 170);
 
 threeml::Renderer renderer(&display, nullptr);
 
-auto drawTask = Task(
-    "Draw Task", 5000, 1, +[]() {
-      uint32_t t = 0;
-      TickType_t wakeTime = xTaskGetTickCount();
+auto drawTask = Task("Draw Task", 5000, 1, +[]() {
+    uint32_t t = 0;
+    TickType_t wakeTime = xTaskGetTickCount();
 
-      while (1) {
+    while (1) {
         renderer.render();
         // while (!display.done_refreshing());
         // display.clear();
@@ -61,25 +60,23 @@ auto drawTask = Task(
         // display.refresh();
 
         vTaskDelayUntil(&wakeTime, pdMS_TO_TICKS(17));
-      }
-    });
+    }
+});
 
 #else
 
 // Basic version `draw` controls LED_BUILTIN
-auto drawTask = Task(
-    "Draw Task", 5000, 1, +[]() {
-      static uint32_t t = 0;
-      while (1) {
+auto drawTask = Task("Draw Task", 5000, 1, +[]() {
+    static uint32_t t = 0;
+    while (1) {
         ledcWrite(BASIC_LEDC_CHANNEL, 128 + 127 * sin(t / 120.f * 2 * PI));
         ++t;
-      }
-    });
+    }
+});
 
 #endif
 
-auto imuTask = Task(
-    "IMU Polling", 5000, 1, +[](const uint16_t freq) {
+auto imuTask = Task("IMU Polling", 5000, 1, +[](const uint16_t freq) {
     const TickType_t delayTime = pdMS_TO_TICKS(1000 / freq);
     Orientation cur;
     if (!BNO086::init()) {
@@ -96,13 +93,12 @@ auto imuTask = Task(
             TaskPrint().printf("Roll: % 7.2f, Pitch: % 7.2f, Yaw: % 7.2f\n", cur.roll, cur.pitch, cur.yaw);
         }
         vTaskDelay(pdMS_TO_TICKS(10));
-      }
-    });
+    }
+});
 
-auto touchTask = Task(
-    "Touch Reporting", 4000, 1, +[]() {
-      TouchPads::init<TOUCH_PAD_NUM1, TOUCH_PAD_NUM2>(60000);
-      while (1) {
+auto touchTask = Task("Touch Reporting", 4000, 1, +[]() {
+    TouchPads::init<TOUCH_PAD_NUM1, TOUCH_PAD_NUM2>(60000);
+    while (1) {
         static uint32_t result1;
         touch_pad_read_raw_data(TOUCH_PAD_NUM1, &result1);
         TaskPrint().printf("Touch State: %i %i\n",
@@ -110,33 +106,35 @@ auto touchTask = Task(
             TouchPads::status[TOUCH_PAD_NUM2]
         );
         if (mouseInitialized && TouchPads::status[TOUCH_PAD_NUM1]) {
-          mouse.press(MOUSE_LEFT);
-        } else if (mouseInitialized) {
-          mouse.release(MOUSE_LEFT);
+            mouse.press(MOUSE_LEFT);
+        }
+        else if (mouseInitialized) {
+            mouse.release(MOUSE_LEFT);
         }
         vTaskDelay(500);
-      }
-    });
+    }
+});
 
 void treeList(File &dir, int level = 0) {
-  if (!dir || !dir.isDirectory()) {
-    USBSerial.println("treeList called on non-directory");
-    return;
-  }
-  File file = dir.openNextFile();
-  while (file) {
-    if (file.isDirectory()) {
-      for (int i = 0; i < level; ++i)
-        USBSerial.print("  ");
-      USBSerial.printf("%s:\n", file.name());
-      treeList(file, level + 1);
-    } else {
-      for (int i = 0; i < level; ++i)
-        USBSerial.print("  ");
-      USBSerial.println(file.name());
+    if (!dir || !dir.isDirectory()) {
+        USBSerial.println("treeList called on non-directory");
+        return;
     }
-    file = dir.openNextFile();
-  }
+    File file = dir.openNextFile();
+    while (file) {
+        if (file.isDirectory()) {
+            for (int i = 0; i < level; ++i)
+                USBSerial.print("  ");
+            USBSerial.printf("%s:\n", file.name());
+            treeList(file, level + 1);
+        }
+        else {
+            for (int i = 0; i < level; ++i)
+                USBSerial.print("  ");
+            USBSerial.println(file.name());
+        }
+        file = dir.openNextFile();
+    }
 }
 
 void getTaskLog(std::vector<const char*>& args) {
@@ -184,39 +182,38 @@ void toggleMonitor(std::vector<const char*>& args) {
 }
 
 void systemStatus(std::vector<const char *> &args) {
-  if (!args.empty()) {
-    USBSerial.println("Expected no arguments");
-    return;
-  }
-  USBSerial.printf("Free heap: %i\n", xPortGetFreeHeapSize());
-  USBSerial.printf("Minimum free heap reached: %i\n",
-                   esp_get_minimum_free_heap_size());
+    if (!args.empty()) {
+        USBSerial.println("Expected no arguments");
+        return;
+    }
+    USBSerial.printf("Free heap: %i\n", xPortGetFreeHeapSize());
+    USBSerial.printf("Minimum free heap reached: %i\n", esp_get_minimum_free_heap_size());
 }
 
 void helpMePlz(std::vector<const char *> &args) {
-  if (!args.empty()) {
-    USBSerial.println("Help for specific commands is NYI.");
-  }
-  USBSerial.println("Registered commands:");
-  for (const std::pair<std::string, Shell::Command> &p : Shell::registry())
-    USBSerial.printf(" - %s\n", p.first.c_str());
+    if (!args.empty()) {
+        USBSerial.println("Help for specific commands is NYI.");
+    }
+    USBSerial.println("Registered commands:");
+    for (const std::pair<std::string, Shell::Command> &p : Shell::registry())
+        USBSerial.printf(" - %s\n", p.first.c_str());
 }
 
 void setup() {
-  // Serial.begin(115200);
+    // Serial.begin(115200);
 
 #ifdef DEBUG
-  // while(!Serial) delay(10); // Wait for Serial to become available.
-  // Necessary for boards with native USB (like the SAMD51 Thing+).
-  // For a final version of a project that does not need serial debug (or a USB
-  // cable plugged in), Comment out this while loop, or it will prevent the
-  // remaining code from running.
+    // while(!Serial) delay(10); // Wait for Serial to become available.
+    // Necessary for boards with native USB (like the SAMD51 Thing+).
+    // For a final version of a project that does not need serial debug (or a USB
+    // cable plugged in), Comment out this while loop, or it will prevent the
+    // remaining code from running.
 #endif
 
-  auto dom = threeml::clean_dom(
-      threeml::parse_string("<head><title>Test</title></head><body><h1>Hello, "
-                            "Mouseless World!</h1>And hello to you, too!</body>"));
-  renderer.set_dom(dom);
+    auto dom = threeml::clean_dom(
+        threeml::parse_string("<head><title>Test</title></head><body><h1>Hello, "
+                              "Mouseless World!</h1>And hello to you, too!</body>"));
+    renderer.set_dom(dom);
 
     Shell::init();
 
@@ -225,31 +222,30 @@ void setup() {
     Shell::registerCmd("memory", systemStatus);
     Shell::registerCmd("help", helpMePlz);
 
-  initUSB();
-  serialState = initSerial();
-  initMSC();
+    initUSB();
+    serialState = initSerial();
+    initMSC();
 
 #ifdef PRO_FEATURES
-  display.init();
-  display.setTextWrap(false);
+    display.init();
+    display.setTextWrap(false);
 
-  display.setTextColor(color_rgb(255, 255, 255));
-  display.setTextSize(2);
+    display.setTextColor(color_rgb(255, 255, 255));
+    display.setTextSize(2);
 #else
-  ledcSetup(BASIC_LEDC_CHANNEL, 1000, 8);
-  ledcAttachPin(LED_BUILTIN, BASIC_LEDC_CHANNEL);
+    ledcSetup(BASIC_LEDC_CHANNEL, 1000, 8);
+    ledcAttachPin(LED_BUILTIN, BASIC_LEDC_CHANNEL);
 #endif
-  drawTask();
+    drawTask();
 
-  while (!USBSerial)
-    ;
-  duk = duk_create_heap_default();
-  duk_push_c_function(duk, native_print, 1);
-  duk_put_global_string(duk, "print");
-  duk_eval_string(duk, "var fib = function(n){return n < 2 ? n : fib(n-2) + "
-                       "fib(n-1)}; print(fib(6));");
-  USBSerial.println((int)duk_get_int(duk, -1));
-  duk_destroy_heap(duk);
+    while (!USBSerial);
+    duk = duk_create_heap_default();
+    duk_push_c_function(duk, native_print, 1);
+    duk_put_global_string(duk, "print");
+    duk_eval_string(duk, "var fib = function(n){return n < 2 ? n : fib(n-2) + "
+                         "fib(n-1)}; print(fib(6));");
+    USBSerial.println((int)duk_get_int(duk, -1));
+    duk_destroy_heap(duk);
 
     if (FFat.begin(false)) {
         File root = FFat.open("/");
@@ -263,4 +259,6 @@ void setup() {
     imuTask(10);
 }
 
-void loop() {}
+void loop() {
+    
+}
