d93c857188935813211cc2c52eff1f2e1ac62ceb
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2024-04-20 15:05:05 -0500

    Added result_t (might get removed)

--- include/meta.h
+++ include/meta.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "usb_classes.h"
+
 namespace meta {
 
 template <typename... Ts>
@@ -17,5 +19,25 @@ template <typename... Ts, typename F>
 void for_each(F func, Ts&&... args) {
     uint8_t result[sizeof...(Ts)] = {(func(std::forward<Ts>(args)), uint8_t(0))...};
 }
+
+template <typename V, typename E> struct result_t {
+    union {
+        V value;
+        E error;
+    };
+    bool has_error;
+
+    result_t(V value) : value(value), has_error(false) {}
+    result_t(E error) : error(error), has_error(true) {}
+
+    V unwrap() {
+        if (has_error) {
+            USBSerial.printf("Unwrapped error in task %s\n. Aborting task.",
+                             pcTaskGetName(nullptr));
+            vTaskDelete(nullptr);
+        }
+        return value;
+    }
+};
 
 }; // end namespace meta
