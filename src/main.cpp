ca410ea6f8fcdc2d978b3e8775d399e5d4055c42
Will Wang <willstar.515151@gmail.com>
2024-04-21 18:36:20 -0500

    Tested and fixed battery reporting bugs

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,9 @@ void setup() {
   //analogWrite(15, 100);
 }
 
-long tt = 0;
+uint32_t tt = 0;
 byte level = 255;
+uint8_t getBatteryPercentage();
 void loop() {
   // put your main code here, to run repeatedly:
   static uint32_t t = 0;
@@ -31,40 +32,60 @@ void loop() {
   display.clear();
 
   // Draw code goes between `display.clear()` and `display.refresh()`
-  display.setCursor(10, 80);
-  display.print("Hello, Mouseless World!");
+  //display.setCursor(10, 80);
+  //display.print("Hello, Mouseless World!");
   display.setCursor(10, 110);
-  display.printf("Frame %i", ++t);
+  display.printf("Frame %i %i", ++t, millis());
   display.setCursor(10, 140);
   //digitalWrite(14, HIGH);
   //delay(10);
 
-  float measurement = (float) analogRead(4);
-  float battery_voltage = (measurement / 4095.0) * 7.26;
-  display.printf("%f %f %d", measurement, battery_voltage, getBatteryPercentage());
+  //display.printf("%f %f %d", measurement, battery_voltage, getBatteryPercentage());
+  uint8_t batterylevel = getBatteryPercentage();
+  if(batterylevel == 101) {
+    display.printf("Battery: Charging\n");
+  }
+  else {
+    display.printf("Battery: %i\n", batterylevel);
+  }
+  
   //Serial.println(bat);
   //digitalWrite(14, LOW);
   
 
-  display.refresh();
   //putting on a test load
-  if(millis() - tt >= 5000) {
+  /*
+  if(millis() - tt >= 5000LL) {
     tt = millis();
     if(level != 255) {
       level = 255;
-      analogWrite(38, level);
+      analogWrite(38, 1000);
     } else {
       level = 50;
       analogWrite(38, level);
     }
   }
+  */
+  display.refresh();
 }
-const uint8_t max_drop_cnt = 2;
+const uint8_t max_drop_cnt = 200; //adjust
 uint8_t getBatteryPercentage() {
   float battery_voltage = (analogRead(4) / 4095.0) * 7.26;
-  static uint8_t filtered_level = 101;
+  //static uint8_t filtered_level = 101;
+  static uint8_t filtered_level = 100;
   static uint8_t drop_cnt = 0;
+  static bool stable_timeout = 0;
   uint8_t level = 100; 
+  //display.setCursor(10, 80);
+  //display.printf("%d %d %d \n", filtered_level, level, drop_cnt);
+
+  //handle charging
+  if(battery_voltage >= 4.7) {
+    filtered_level = 101;
+    drop_cnt = 0;
+    level = 101;
+    return 101;
+  }
   //https://blog.ampow.com/lipo-voltage-chart/
   if(battery_voltage >= 4.2) {
     level = 100;
@@ -92,20 +113,40 @@ uint8_t getBatteryPercentage() {
     //critical level
     level = 0;
   }
+  //display.setCursor(10, 80);
+  //display.printf("%d %d %d \n", filtered_level, level, drop_cnt);
+  if(!stable_timeout) {
+    //the battery tends to have a voltage drop whenever the board is first powered
+    //this delay gives the battery a little time to stabilize before remembering the measurements
+    //the sensors and screens should be initialized before this timeout expires
+    if(millis() >= 5000) {
+      stable_timeout = 1;
+    } else {
+      filtered_level = level;
+      return level;
+    }
+  }
+  /*
   if(filtered_level == 101) {
     filtered_level = level;
   }
-  if(level < filtered_level) {
-    ++drop_cnt;
-    level = filtered_level;
+  */
+  if(level >= filtered_level) {
+    //if the level momentarily goes above the stable level 
+    //we will reset the times it dropped below the stable level
+    drop_cnt = 0;
   }
   if(drop_cnt >= max_drop_cnt) {
     //drop down
     drop_cnt = 0;
     filtered_level = level;
   }
+  if(level < filtered_level) {
+    ++drop_cnt;
+    level = filtered_level;
+  }
   if(filtered_level < level) {
     level = filtered_level;
   }
-  return level;
+  return level; //returns 101 if charging
 }
